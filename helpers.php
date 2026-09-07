9e0a97bb6062365bd9503ffb7242d49e2741b468
she4nwrld <sheeneprestin72@gmail.com>
2026-09-07 20:26:58 +0800

    move escaping and google mark helpers into helpers.php

---
+++ helpers.php
@@ -0,0 +1,18 @@
+<?php
+
+// shortcut lang ni para dili ko mag-type ug htmlspecialchars kada higayon
+function e($text): string
+{
+    return htmlspecialchars((string) $text, ENT_QUOTES, 'UTF-8');
+}
+
+// google mark, gisulat usa ka beses lang instead nga unom ka beses sa markup
+function googleMark(): string
+{
+    return '<svg viewBox="0 0 48 48" aria-hidden="true">'
+        . '<path fill="#FFC107" d="M43.6 20.1H42V20H24v8h11.3C33.7 32.7 29.2 36 24 36c-6.6 0-12-5.4-12-12s5.4-12 12-12c3.1 0 5.8 1.2 7.9 3l5.7-5.7C34 6.1 29.3 4 24 4 13 4 4 13 4 24s9 20 20 20 20-9 20-20c0-1.3-.1-2.6-.4-3.9z"/>'
+        . '<path fill="#FF3D00" d="M6.3 14.7l6.6 4.8C14.7 15.1 19 12 24 12c3.1 0 5.8 1.2 7.9 3l5.7-5.7C34 6.1 29.3 4 24 4 16.3 4 9.7 8.3 6.3 14.7z"/>'
+        . '<path fill="#4CAF50" d="M24 44c5.2 0 9.9-2 13.4-5.2l-6.2-5.2C29.3 35.1 26.7 36 24 36c-5.2 0-9.6-3.3-11.3-7.9l-6.5 5C9.5 39.6 16.2 44 24 44z"/>'
+        . '<path fill="#1976D2" d="M43.6 20.1H42V20H24v8h11.3c-.8 2.2-2.2 4.1-4.1 5.6l6.2 5.2C37.8 39.2 44 34.5 44 24c0-1.3-.1-2.6-.4-3.9z"/>'
+        . '</svg>';
+}
